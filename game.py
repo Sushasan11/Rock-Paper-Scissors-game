8892403b70fdf80aed338cd4a32ff5e88605bd6c
Sushasan <108333646+Sushasan11@users.noreply.github.com>
2024-02-10 09:27:16 +0545

    commit

---
+++ game.py
@@ -0,0 +1,29 @@
+import random
+#Infinite loop to keep the game running
+while True: 
+    choose = ["rock", "paper", "scissors"]
+    system = random.choice(choose)
+    user = input('User Choice: ').lower()
+    
+    # Check if user input is valid or not
+    if user not in choose: 
+        print('Error: Invalid input!!')
+        # Prompt user again if input is valid
+        continue 
+    
+    print('Computer:', system)
+    
+    if user == system:
+        print('Tie!!')
+        
+    elif (user == 'rock' and system == 'scissors') or (user =='paper' and system =='rock') or (user =='scissors' and system == 'paper'):
+        print('User Wins')
+        
+    else:
+        print('Computer Wins') 
+        
+# Asks user is they want to play again
+    play_again = input('Paly Again ? (y/n): ')
+    if play_again != 'y':
+        print('Thanks for playing')
+        break
